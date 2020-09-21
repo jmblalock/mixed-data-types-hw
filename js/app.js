bd9e42705c5cbbcbd053cbce514de784bd6f1386
j-blalock <justin.m.blalock@gmail.com>
2020-09-20 21:17:19 -0500

    bondTitles array

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ then give a small example of the **data structure**:
     created.
 */
 
-// 1.
+// 2-1.
 rainbowArray = [
     'red',
     'orange',
@@ -98,11 +98,11 @@ rainbowArray = [
     'violet'
 ]
 
-// 2.
+// 2-2.
 const blue = rainbowArray[4];
 console.log(blue);
 
-// 3.
+// 2-3.
 const justin = {
     favoriteFood: 'bbq',
     hobby: 'snowboarding',
@@ -110,7 +110,7 @@ const justin = {
     favoriteDataType: 'number'
 }
 
-// 4.
+// 2-4.
 const jHobby = justin.hobby;
 console.log(jHobby);
 
@@ -157,19 +157,19 @@ const crazyObject = {
   }
 }
 
-// 1.
+// 3-1.
 console.log(crazyObject.taco[1].salsa[5]);
 
-// 2.
+// 3-2.
 console.log(crazyObject.larry.quotes[0]);
 
-// 3. 
+// 3-3. 
 console.log(crazyObject.larry.characters[2].favourtieHobby);
 
-// 4.
+// 3-4.
 console.log(crazyObject.larry.nicknames[1]);
 
-// 5.
+// 3-5.
 console.log(crazyObject.larry.characters[1]);
 
 /*
@@ -200,3 +200,54 @@ const inception = {
 
 inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.
 dreamLayer5.dreamLayer6.limbo = null;
+
+/*
+### 5-7. Bond Films
+
+Copy the following `bondFilms` **array** of **objects** above into your js 
+file. Use loops and conditionals and functions in order to complete the below:
+
+5.  Create a new array called `bondTitles` with only the titles of the Bond 
+    films, and console.log the new array.
+
+6.  Create a new array `oddBonds`, of only the Bond films released on 
+    odd-numbered years.
+
+7.  Determine the total cumulative gross of the Bond franchise, and console.log
+    the result.
+*/
+
+const bondFilms = [
+    { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+    { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+    { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+    { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+    { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+    { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+    { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+    { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+    { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+    { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+    { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+    { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+    { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+    { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+    { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+    { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+    { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+    { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+    { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+    { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+    { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+    { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+    { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+]
+
+// 5.
+const bondTitles = [];
+
+for (const i in bondFilms) {
+    bondTitles.push(bondFilms[i]["title"]);
+}
+
+console.log(bondTitles);
